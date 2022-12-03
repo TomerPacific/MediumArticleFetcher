d50556762da29ac660d0af0b482ed61478796475
unknown <tomerpacific@gmail.com>
2022-12-03 19:05:41 +0200

    MediumArticleFetcher adding ErrorMessage component

--- MediumArticleFetcher/src/App.svelte
+++ MediumArticleFetcher/src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Spinner from './lib/Spinner.svelte';
   import UserInput from './lib/UserInput.svelte';
+  import ErrorMessage from './lib/ErrorMessage.svelte';
 </script>
 
 <main id="conatiner">
   <UserInput />
   <Spinner />
+  <ErrorMessage />
 </main>
 
 <style>

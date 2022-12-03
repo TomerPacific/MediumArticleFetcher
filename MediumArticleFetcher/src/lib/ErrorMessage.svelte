e8a00cfbd7b98ad97be85fa1dcff04cb22a4b7e9
unknown <tomerpacific@gmail.com>
2022-12-03 19:03:48 +0200

    MediumArticleFetcher creating component for error message

---
+++ MediumArticleFetcher/src/lib/ErrorMessage.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { appStore } from './AppStore';
+    import { onMount } from 'svelte';
+
+    let errorMessageDiv
+
+    onMount(() => {
+        appStore.subscribe(data => {
+            if (data.errorMessage.length > 0) {
+                errorMessageDiv.style.display = "inline-block"
+                errorMessageDiv.innerHTML = data.errorMessage
+            } else {
+                errorMessageDiv.style.display = "none"
+            }
+      })
+    })
+</script>
+
+
+<main>
+    <h3 id="errorMessage" bind:this={errorMessageDiv}>
+    </h3>
+</main>

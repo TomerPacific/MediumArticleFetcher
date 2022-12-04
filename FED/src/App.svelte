3c1a0246681a6a55cd8a292c9f291963cc04688a
unknown <tomerpacific@gmail.com>
2022-12-04 21:35:01 +0200

    MediumArticleFetcher adding user profile element

--- FED/src/App.svelte
+++ FED/src/App.svelte
@@ -2,10 +2,12 @@
   import Spinner from './Spinner.svelte';
   import UserInput from './UserInput.svelte';
   import ErrorMessage from './ErrorMessage.svelte';
+  import UserProfile from './UserProfile.svelte';
 </script>
 
 <main id="container">
     <UserInput />
     <Spinner />
     <ErrorMessage />
+    <UserProfile />
   </main>
